f35fed63fceb232857d1542639e371a71e4fbebb
Marion Saru <maghangasaru@gmail.com>
2025-07-08 20:02:15 +0300

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+
+    if length == 0:
+        print(fibonacci)
+    elif length == 1:
+        fibonacci.append(0)
+        print(fibonacci)
+    else:
+        fibonacci = [0, 1]
+        while len(fibonacci) < length:
+            next_number = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(next_number)
+        print(fibonacci)
+
+print_fibonacci(4)        
